34796c4dd90dcd43113fe2df6409682ba33442fa
xWinst <36066519+xWinst@users.noreply.github.com>
2022-06-07 11:14:18 +0000

    Deploying to gh-pages from @ xWinst/hellen_test@8c2566ff40610780311184656965f28d74f98e88 🚀

---
+++ index.d2f13386.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{modules:[myPlugin],pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},debugger:!0});
+//# sourceMappingURL=index.d2f13386.js.map
