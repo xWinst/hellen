9ef46b4434656dea812301591744aec3b224f011
Maryna Ushakova <ginger4795@gmail.com>
2022-06-07 10:54:05 +0300

    add footer

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,14 +2,14 @@
   const refs = {
     openMenuBtn: document.querySelector('.menu-open-btn'),
     closeMenuBtn: document.querySelector('.menu-close-btn'),
-    // closeMenu: document.querySelector('.mobile-menu-link'),
+    closeMenu: document.querySelectorAll('.mobile-menu-link'),
     menu: document.querySelector('.mobile-menu'),
     body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  // refs.closeMenu.addEventListener('click', toggleMenu);
+  refs.closeMenu.forEach(link => link.addEventListener('click', toggleMenu));
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
