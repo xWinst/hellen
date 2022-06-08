2d6fd9a0c21e4791e4c4a65be38f053c88be6c6d
Oleg <hellreebok@gmail.com>
2022-06-08 16:40:49 +0300

    update mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('.menu-open-btn'),
     closeMenuBtn: document.querySelector('.menu-close-btn'),
     closeMenu: document.querySelectorAll('.mobile-menu-link'),
+    closeMenu2: document.querySelectorAll('[data-modal-open2]'),
     menu: document.querySelector('.mobile-menu'),
     body: document.querySelector('body'),
   };
@@ -10,6 +11,7 @@
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenu.forEach(link => link.addEventListener('click', toggleMenu));
+  refs.closeMenu2.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
