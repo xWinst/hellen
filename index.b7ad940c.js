64d5d4277b31fd89ebfd26ed8abafa9672baefd4
xWinst <36066519+xWinst@users.noreply.github.com>
2022-06-08 13:45:03 +0000

    Deploying to gh-pages from @ xWinst/hellen_test@98e710c00615305dbd1541ad3081ab0b5c5c8251 🚀

--- index.b7ad940c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),closeMenu:document.querySelectorAll(".mobile-menu-link"),menu:document.querySelector(".mobile-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n),e.closeMenu.forEach((function(e){return e.addEventListener("click",n)}))}();
-//# sourceMappingURL=index.b7ad940c.js.map
