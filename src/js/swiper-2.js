7b8cc9d60671bd67e8180b865e09262f24ebf860
Mykhailo Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2022-06-08 12:34:26 +0300

    fix icon

--- src/js/swiper-2.js
+++ src/js/swiper-2.js
@@ -1,8 +1,11 @@
+const mediaQueryList = window.matchMedia("(max-width:767px)");
+const test = mediaQueryList.matches? true:false;
 var swiper = new Swiper('.swiper', {
   modules: [myPlugin],
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+    dynamicBullets: test,
   },
   navigation: {
     nextEl: '.swiper-button-next',
