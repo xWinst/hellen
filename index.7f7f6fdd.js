b9f2ddc8f46a30669418527261c4e37a5e5a43f2
xWinst <36066519+xWinst@users.noreply.github.com>
2022-06-08 09:48:55 +0000

    Deploying to gh-pages from @ xWinst/hellen_test@9776c3d9f8dd009555814bdbc6fe4c16eca48a98 🚀

---
+++ index.7f7f6fdd.js
@@ -0,0 +1,2 @@
+var mediaQueryList=window.matchMedia("(max-width:767px)"),test=!!mediaQueryList.matches,swiper=new Swiper(".swiper",{modules:[myPlugin],pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:test},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},debugger:!0});
+//# sourceMappingURL=index.7f7f6fdd.js.map
