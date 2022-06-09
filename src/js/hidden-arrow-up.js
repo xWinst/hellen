3286a734cd1f8481d9594508d27a778650e478b9
Oleg <hellreebok@gmail.com>
2022-06-09 13:21:18 +0300

    add animation

---
+++ src/js/hidden-arrow-up.js
@@ -0,0 +1,13 @@
+// (() => {
+//     const refs = {
+//         header: document.querySelector('.header'),
+//         upbutton: document.querySelector('[upbutton]'),
+//     };
+
+//     window.addEventListener("scroll", showArrow);
+
+//     function toggleMenu() {
+//     if (refs.header.scrollTop > 100) refs.upbutton.add('is-hidden');
+//     else refs.upbutton.remove('is-hidden');
+
+//   })();
