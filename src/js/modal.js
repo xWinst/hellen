0c46b9a6a786ff7ca62590e1c5e32866a0119b7b
Oleg <hellreebok@gmail.com>
2022-06-09 20:41:38 +0300

    add certificate

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,15 +2,24 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     openModalBtn2: document.querySelector('[data-modal-open2]'),
+    openCertificate: document.querySelector('.about__link'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    certClose: document.querySelector('[certClose]'),
     modal: document.querySelector('[data-modal]'),
+    certificate: document.querySelector('[certificate]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openCertificate.addEventListener('click', toggleCert);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.certClose.addEventListener('click', toggleCert);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  }
+
+  function toggleCert() {
+    refs.certificate.classList.toggle('is-hidden');
   }
 })();
